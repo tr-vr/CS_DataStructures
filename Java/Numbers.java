15fad1628acd49a946e94409bea2a5ea3d500080
tr-vr <140265392+tr-vr@users.noreply.github.com>
2024-05-18 22:04:41 +1000

    Added Numbers.java file This file iterates over 0 -> 9 summing the total

---
+++ Java/Numbers.java
@@ -0,0 +1,25 @@
+package Java;
+
+/**
+ * Numbers
+ */
+public class Numbers {
+
+    public static void main(String[] args) {
+        int x = 0;
+        int sum = 0;
+        while (x < 10) {
+            System.out.print(sum + " ");
+            x = x + 1;
+            sum += x;
+        }
+        System.out.println();
+    }
+}
+
+/* Variable x must be declared before it can be used 
+ * Loop definition is contained inside of curly braces, and the boolean expression that is 
+ * contained inside of parentheses.
+ * print(...) without the 'ln' means that a new line will not be included
+
+*/
